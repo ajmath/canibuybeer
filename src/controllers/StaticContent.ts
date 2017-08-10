4e3c6ddeae91eba31c671ef3bea6cd952fa4cf16
Andrew Matheny <andrew.j.matheny@gmail.com>
2017-08-10 13:31:04 -0400

    Remove debug code in static content

--- src/controllers/StaticContent.ts
+++ src/controllers/StaticContent.ts
@@ -1,12 +1,6 @@
-import {ReplyNoContinue, RouteConfiguration, Request} from "hapi";
+import {RouteConfiguration} from "hapi";
 
 export default class StaticContent {
-
-  public getFile(request: Request, reply: ReplyNoContinue) {
-    console.log(`Got request for param ${request.params.param}:  Returning index.html`);
-    return reply.file("index.html");
-  }
-
   public static getRoutes(): RouteConfiguration[] {
     // const controller = new StaticContent();
     return [{
